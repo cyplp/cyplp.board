9d91a586ac60d5368fc3ec2039aa80416b5bf12a
Cyprien Le Pannérer <cyplp@free.fr>
2015-02-28 16:15:22 +0100

    fake data

--- cyplp/board/views.py
+++ cyplp/board/views.py
@@ -3,4 +3,61 @@ from pyramid.view import view_config
 
 @view_config(route_name='home', renderer='templates/mytemplate.pt')
 def my_view(request):
-    return {'project': 'cyplp.board'}
+
+    columns = [{'title': 'TODO',
+                '_id': 'ertyuiop',
+                'items': [{'title': 'item1',
+                           'content': 'rtyuibn,isdfsdfefdsfsdfd'},
+                           {'title': 'item1',
+                           'content': 'rtyuibn,isdfsdfefdsfsdfd'},
+                           {'title': 'item1',
+                           'content': 'rtyuibn,isdfsdfefdsfsdfd'},
+                           {'title': 'item1',
+                           'content': 'rtyuibn,isdfsdfefdsfsdfd'}]},
+                {'title': 'Doing',
+                '_id': 'ertyuiop',
+                'items': [{'title': 'item1',
+                           'content': 'rtyuibn,isdfsdfefdsfsdfd'},
+                           {'title': 'item1',
+                           'content': 'rtyuibn,isdfsdfefdsfsdfd'},
+                           {'title': 'item1',
+                           'content': 'rtyuibn,isdfsdfefdsfsdfd'},
+                           {'title': 'item1',
+                           'content': 'rtyuibn,isdfsdfefdsfsdfd'}]},
+
+                {'title': 'Pause',
+                '_id': 'ertyuiop',
+                'items': [{'title': 'item1',
+                           'content': 'rtyuibn,isdfsdfefdsfsdfd'},
+                           {'title': 'item1',
+                           'content': 'rtyuibn,isdfsdfefdsfsdfd'},
+                           {'title': 'item1',
+                           'content': 'rtyuibn,isdfsdfefdsfsdfd'},
+                           {'title': 'item1',
+                           'content': 'rtyuibn,isdfsdfefdsfsdfd'}]},
+                {'title': 'Done',
+                '_id': 'ertyuiop',
+                'items': [{'title': 'item1',
+                           'content': 'rtyuibn,isdfsdfefdsfsdfd'},
+                           {'title': 'item1',
+                           'content': 'rtyuibn,isdfsdfefdsfsdfd'},
+                           {'title': 'item1',
+                           'content': 'rtyuibn,isdfsdfefdsfsdfd'},
+                           {'title': 'item1',
+                           'content': 'rtyuibn,isdfsdfefdsfsdfd'}]},
+                {'title': 'Nope',
+                '_id': 'ertyuiop',
+                'items': [{'title': 'item1',
+                           'content': 'rtyuibn,isdfsdfefdsfsdfd'},
+                           {'title': 'item1',
+                           'content': 'rtyuibn,isdfsdfefdsfsdfd'},
+                           {'title': 'item1',
+                           'content': 'rtyuibn,isdfsdfefdsfsdfd'},
+                           {'title': 'item1',
+                           'content': 'rtyuibn,isdfsdfefdsfsdfd'}]}
+
+                ]
+
+    return {'columns': columns,
+            'board': {'name': 'my first board'},
+            }
