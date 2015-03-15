cdcf4acde050824696dc451001b39e7998076e98
Cyprien Le Pannérer <cyplp@free.fr>
2015-03-15 17:20:16 +0100

    DnD working

--- cyplp/board/static/board.js
+++ cyplp/board/static/board.js
@@ -8,6 +8,31 @@ document.addEventListener("DOMContentLoaded", function(event) {
 	    addItems[item].addEventListener('click', function(e) {eventAddItems(e)});
 	}
 
+    var items = document.querySelectorAll('.item');
+    for (var item=0, len=items.length; item<len; item++)
+	{
+	    items[item].addEventListener('dragstart', function(e){
+								  e.dataTransfer.effectAllowed = "move";
+								  e.dataTransfer.setData("text", e.target.id);
+								 });
+	}
+
+    var columns = document.querySelectorAll('.column');
+    for(var item=0, len=columns.length; item<len; item++)
+	{
+	    columns[item].addEventListener('drop', function(event){
+		console.log('drop');
+		event.preventDefault();
+		id = event.dataTransfer.getData("text");
+
+		target = event.target;
+		if (target.class="column"){
+		    target.appendChild(document.getElementById(id))
+		}
+	    });
+	    columns[item].addEventListener('dragover', function(e){
+	     							   e.preventDefault();});
+	}
   });
 
 function eventAddItems(evt)
