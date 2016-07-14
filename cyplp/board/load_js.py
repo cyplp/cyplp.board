b251335b3ac8d99e4d1f29c82b047c7b06e783d2
Cyprien Le Pannérer <cyplp@free.fr>
2016-07-14 14:54:57 +0200

    quick handle of conflict

--- cyplp/board/load_js.py
+++ cyplp/board/load_js.py
@@ -5,12 +5,15 @@ import os.path
 from pyramid.events import ApplicationCreated
 from pyramid.events import subscriber
 
+import pycouchdb
 
 @subscriber(ApplicationCreated)
 def on_launch(event):
     db = event.app.registry.db
-
     for view in glob.glob(os.path.join(os.path.dirname(__file__), 'data', '*.js')):
         with open(view, 'r') as current:
             doc = json.load(current)
-            db.save(doc)
+            try:
+                db.save(doc)
+            except pycouchdb.exceptions.Conflict:
+                pass
